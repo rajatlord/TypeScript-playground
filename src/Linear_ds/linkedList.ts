2c8b608b2830c6768195cdd811fb098af2f01d16
rajatlord <rajatmathur1881@gmail.com>
2026-04-24 11:16:02 +0530

    LinkedList

--- src/Linear_ds/linkedList.ts
+++ src/Linear_ds/linkedList.ts
@@ -34,6 +34,38 @@ class LinkedList {
     }
     return size;
   }
+
+  public pushNode(data:number):void{
+    if(this.head === null){
+      this.head = new ListNode(data);
+    }else{
+      let temp = this.head;
+      while(temp.next !== null){
+        temp = temp.next;
+      }
+      temp.next = new ListNode(data);
+    }
+  }
+
+  // for inserting node head
+  public insertHead(data:number):void{
+    let node = new ListNode(data);
+    let temp = this.head;
+    this.head = node;
+    this.head.next = temp;
+  }
+
+  // finding in the linkedlist
+  public find(value:number):boolean{
+    let temp = this.head;
+    while(temp!==null){
+      if(temp.data === value){
+        return true;
+      }
+      temp = temp.next;
+    }
+    return false;
+  }
 }
 
 let nodeOne = new ListNode(5);
@@ -42,8 +74,14 @@ let nodeThree = new ListNode(25);
 
 nodeOne.next = nodeTwo;
 nodeTwo.next = nodeThree;
-
+// let nodeFour = new ListNode(30);
 let linkedList = new LinkedList(nodeOne);
+linkedList.pushNode(30)
+
+// inserting head
+linkedList.insertHead(38);
+
 console.log(linkedList);
 console.log(linkedList.length());
+console.log("is 38 in the list? ",linkedList.find(38));
 // linkedList.print();
